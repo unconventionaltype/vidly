574e0bf1c21f8f8e643f6ef83ec34e2103d633c7
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro.local>
2022-09-15 05:25:51 -0600

    first get route

--- index.js
+++ index.js
@@ -2,4 +2,27 @@ const Joi = require('joi');
 const express = require('express');
 
 const app = express();
+const date = new Date();
 
+//fake database
+const genres = {}
+
+//routes
+app.get('/api/genres', (req, res) => {
+    res.send(genres)
+});
+
+
+//input validation function
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
+
+//open port listed in environment var PORT or 3000
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Lisening on port ${port} ${date}...`)
+});
